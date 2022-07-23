a48b4f5db23a8fec45d645c125838063534b5217
Nigel Davis <ndavis10@uoguelph.ca>
2022-07-23 17:55:38 -0400

    more powershell aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,9 @@
 function ginit ([Parameter(ValueFromRemainingArguments)] $remaining) {
     git init $remaining
 }
+function gremote ([Parameter(ValueFromRemainingArguments)] $remaining) {
+    git remote $remaining
+}
 function gclone ([Parameter(ValueFromRemainingArguments)] $remaining) {
     git clone $remaining
 }
@@ -71,4 +74,53 @@ function gch ($message) {
     git add .
     git commit -m $message
     git push
+}
+function gnb ($branch_name) {
+    gcheckout -b $branch_name
+    gcommit -m "new branch - $($branch_name)"
+    gpush --set-upstream origin $branch_name
+}
+function gstart ([Parameter(Mandatory)] $repo) {
+    ginit
+    gremote add origin $repo
+    gadd .
+    gpush --set-upstream origin master
+}
+function mergemaster {
+    $cur_branch = gbranch --show-current
+    gpull
+    gcheckout master
+    gpull
+    gcheckout $cur_branch
+    gmerge master
+}
+
+# CD
+
+function .. {
+    Set-Location ..
+}
+function ... {
+    Set-Location ../..
+}
+function .... {
+    Set-Location ../../..
+}
+function ..... {
+    Set-Location ../../../..
+}
+function ...... {
+    Set-Location ../../../../..
+}
+
+function projects {
+    Set-Location "C:\Users\ngeld\Projects"
+}
+
+function course ($course) {
+    $path = Get-ChildItem -Path ~/Documents/School -Depth 1 -Directory | 
+        Where-Object {$_.Name -match $course} | 
+        ForEach-Object { Write-Host $_.FullName } 6>&1
+
+    Set-Location $path
 }
